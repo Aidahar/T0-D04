4d346b2dffad3a2234362b3862c9eae0558e6d55
Aidahar <zagirik@gmail.com>
2022-08-12 06:21:41 +0300

    division by substrat

--- src/1948.c
+++ src/1948.c
@@ -1,20 +1,38 @@
-// Copyright [year] <Copyright Owner>
+// Copyright 2022 Trudieis
 #include <stdio.h>
 
 int division(int a, int b);
+void print_number(int x);
 
 int main() {
-    int a, b;
-    scanf("%d%d", &a, &b);
-    printf("%d\n", division(a, b));
+    int a;
+    scanf("%d", &a);
+    print_number(a);
     return 0;
 }
 
 int division(int a, int b) {
-    int result = 0;
-    while (a >= b) {
-        a -= b;
-        result++;
+    if (a < 0) {
+      a *= -1;
     }
-    return result;
+    while (a != 0 && b != 0) {
+      if (a > b) { 
+        a -= b;
+      } else {
+        b -= a;
+      }
+    }
+    return a + b;
+}
+
+void print_number(int x) {
+  int div = 2;
+  while (x != 1) {
+    while (division(x, div) == 0) {
+      printf("divisor %d", div);
+      x = division(x, div);
+    }
+    div += 1;
+  }
+  printf("\n");
 }
