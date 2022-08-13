8c4e6faf14c79ab1218c3d897179d56995c8be68
Aidahar <zagirik@gmail.com>
2022-08-13 10:35:13 +0300

    solution 1984.c

--- src/1948.c
+++ src/1948.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int division(int a, int b);
+int ost(int a, int b);
 void print_number(int x);
 
 int main() {
@@ -15,24 +16,42 @@ int division(int a, int b) {
     if (a < 0) {
       a *= -1;
     }
-    while (a != 0 && b != 0) {
-      if (a > b) { 
-        a -= b;
-      } else {
-        b -= a;
-      }
+    int res = 0, ost = a;
+    while (ost >= b) {
+      res++;
+      ost -= b;
     }
-    return a + b;
+    if (ost == 1) {
+      res = ost;
+    } 
+    return res;
+}
+
+int ost(int a, int b) {
+    if (a < 0) {
+      a *= -1;
+    }
+    int res = 0, ost = a;
+    while (ost >= b) {
+      res++;
+      ost -= b;
+    }
+    if (ost == 1) {
+      res = ost;
+    } else if (ost == 0) {
+      res = 0;
+    }
+    return res;
 }
 
 void print_number(int x) {
   int div = 2;
   while (x != 1) {
-    while (division(x, div) == 0) {
-      printf("divisor %d", div);
+    if (ost(x, div) == 0) {
       x = division(x, div);
+    } else  {
+      div += 1;
     }
-    div += 1;
   }
-  printf("\n");
+  printf("%d\n", div);
 }
